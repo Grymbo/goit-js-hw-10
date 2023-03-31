285c90f74837c7b2490943fc991211f1ee796b96
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2023-03-31 14:21:01 -0600

    se inicia el proyecto

---
+++ src/index.js
@@ -0,0 +1,77 @@
+import "./css/styles.css";
+const _ = require("lodash");
+import Notiflix from "notiflix";
+import fetchCountry from "./countries";
+
+const search = document.querySelector(".search-box");
+const list = document.querySelector(".country-list");
+const DEBOUNCE_DELAY = 300;
+
+// Function para borrar la lista
+const remove = () => {
+    list.innerHTML = "";
+};
+
+// Listener para esccuhar el usuario
+search.addEventListener(
+    "input",
+     _.debounce((e) => {
+        if (e.target.value === "") {
+            remove();
+        }
+
+        let input = e.target.value;
+        input = input.trim();
+        let url = `https://restcountries.com/v2/name/${input}`;
+
+        fetchCountry(url).then((countries) => {
+            renderPosts(countries);
+        }).catch((error) => {
+            Notiflix.Notify.failure("Oops, there is no country with that name");
+        });
+    }, DEBOUNCE_DELAY)
+);
+
+function renderPosts(countries) {
+    if (countries.length == 1) {
+        remove();
+        countrytarget(countries);
+    }
+
+    if (countries.length > 2 && countries.length < 10) {
+        remove();
+        const markup = countries.map((country) => {
+            return `
+                <li><img class="flag" alt="" src="${country.flags.svg}">${country.name}</li>
+            `;
+        }).join("");
+
+    list.innerHTML = markup;
+
+    }
+
+    if (countries.length > 10) {
+        Notiflix.Notify.info(
+            `Too many matches found. Please enter a more specific name.`
+        );
+    }
+}
+
+function countrytarget(countries) {
+    const country = countries[0];
+    let Arraylanguajes = languajes(country.languages);
+    list.innerHTML = `
+        <div class="country__target" >
+            <h1><img  alt="${country.name}" src="${country.flags.svg}">  ${country.name}</h1>
+            <ul>
+                <li>Capital: ${country.capital} </li>
+                <li>Population: ${country.population} </li>
+                <li>Languajes: ${Arraylanguajes}</li>
+            </ul>
+        </div>`;
+}
+
+function languajes(Arraylanguajes) {
+    const languajes = Arraylanguajes.map((e) => e.name);
+    return languajes;
+}
